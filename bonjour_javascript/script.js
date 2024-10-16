89fc28a90c49f1276626a930a922de9cbaaace1e
tanguyjo <tanguyjonqua@gmail.com>
2024-10-16 11:23:27 +0200

    premier Commit Exo individuels

---
+++ bonjour_javascript/script.js
@@ -0,0 +1,24 @@
+
+
+// Je cree ma variable message et lui assigne Bonjour !
+//let firstname = 
+let firstname = prompt("Quel est votre prenom ?");
+
+let message = "Bonjour " + firstname +" !"
+
+//console.log(message)
+
+
+function sayHello(firstname,hour)
+{
+    if (hour < 18 ){
+        document.querySelector('h1').innerText = message;
+        //console.log("Bonjour " + firstname);
+    }
+        else {
+            document.querySelector('h1').innerText = message;
+           // console.log("Bonsoir " + firstname)
+            
+        }
+    }
+sayHello()
