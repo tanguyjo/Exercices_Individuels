b89fa32f38d49cec3ff000d35e89f46aa262e7b3
tanguyjo <jonquatanguy@gmail.com>
2025-01-30 10:33:31 +0100

    Finissions de la calculette

--- calculatrice_graphique/Calc.js
+++ calculatrice_graphique/Calc.js
@@ -14,7 +14,9 @@ function calculer() {
     result = 'Non defini';}
 Shown.value = result;
 }
-
+function deleted() {
+    Shown.value = Shown.value.slice(0, -1);
+}
 // document.getElementById("searchInput").addEventListener("keypress", (e) => {
 //     if (e.key === "Enter") {                           // quand on appuis sur enter ca simule le clic du bouton 
 //       document.getElementById("egal").click(); // Simule un clic sur le bouton
